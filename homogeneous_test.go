7bf4d86690c085db0a08b7db9992fb1781e59574
pwiecz <p@wie.cz>
2019-05-21 13:27:53 +0200

    Add an initial test for homogeneous fielding

---
+++ homogeneous_test.go
@@ -0,0 +1,65 @@
+package main
+
+import "math"
+import "os"
+import "testing"
+
+var portals []Portal
+
+func numPortalsPerDepth(depth uint16) int {
+	return int(math.Pow(3, float64(depth-1)))/2 + 3
+}
+func isCorrectHomogeneous(p0, p1, p2 Portal, depth uint16, portals []Portal) ([]Portal, bool) {
+	if depth == 1 {
+		return portals, true
+	}
+	portal := portals[0]
+	triangle := newTriangleQuery(p0.LatLng, p1.LatLng, p2.LatLng)
+	if !triangle.ContainsPoint(portal.LatLng) {
+		return portals, false
+	}
+	var res bool
+	if portals, res = isCorrectHomogeneous(portal, p1, p2, depth-1, portals[1:]); !res {
+		return portals, false
+	}
+	if portals, res = isCorrectHomogeneous(p0, portal, p2, depth-1, portals); !res {
+		return portals, false
+	}
+	if portals, res = isCorrectHomogeneous(p0, p1, portal, depth-1, portals); !res {
+		return portals, false
+	}
+	return portals, true
+}
+func checkValidHomogeneousResult(expectedDepth uint16, portals []Portal, depth uint16, t *testing.T) {
+	if depth != expectedDepth {
+		t.Errorf("Expected depth %d, actual depth %d", expectedDepth, depth)
+	}
+	if numPortalsPerDepth(depth) != len(portals) {
+		t.Errorf("Expected %d portals for depth %d, got %d portals", numPortalsPerDepth(depth), depth, len(portals))
+		return
+	}
+	if _, ok := isCorrectHomogeneous(portals[0], portals[1], portals[2], depth, portals[3:]); !ok {
+		t.Errorf("Result is not correct homogeneous fielding")
+		return
+	}
+}
+func TestPretty(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	if len(portals) < 3 {
+		t.FailNow()
+	}
+	scorer := newThickTrianglesScorer(len(portals))
+	result, depth := DeepestHomogeneous2(portals, 6, scorer, scorer)
+	checkValidHomogeneousResult(5, result, depth, t)
+}
+
+func TestMain(m *testing.M) {
+	var err error
+	portals, err = ParseJSONFile("portals_test_homogeneous.json")
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(m.Run())
+}
