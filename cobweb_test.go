9a14fe6336a8f84692d0ef3de3d51356d3c2a009
pwiecz <p@wie.cz>
2019-05-21 16:37:20 +0200

    Add an initial test of cobweb fielding

---
+++ cobweb_test.go
@@ -0,0 +1,38 @@
+package main
+
+import "testing"
+
+func isCorrectCobweb(p0, p1, p2 Portal, portals []Portal) bool {
+	if len(portals) == 0 {
+		return true
+	}
+	triangle := newTriangleQuery(p0.LatLng, p1.LatLng, p2.LatLng)
+	if !triangle.ContainsPoint(portals[0].LatLng) {
+		return false
+	}
+	return isCorrectCobweb(p1, p2, portals[0], portals[1:])
+}
+
+func checkValidCobwebResult(expectedLength int, portals []Portal, t *testing.T) {
+	if len(portals) != expectedLength {
+		t.Errorf("Expected length %d, actual length %d", expectedLength, len(portals))
+	}
+	if !isCorrectCobweb(portals[0], portals[1], portals[2], portals[3:]) {
+		t.Errorf("Result is not correct cobweb fielding")
+	}
+}
+
+func TestCobweb(t *testing.T) {
+	portals, err := ParseJSONFile("portals_test_cobweb.json")
+	if err != nil {
+		panic(err)
+	}
+	if testing.Short() {
+		t.Skip()
+	}
+	if len(portals) < 3 {
+		t.FailNow()
+	}
+	cobweb := LargestCobweb(portals)
+	checkValidCobwebResult(34, cobweb, t)
+}
