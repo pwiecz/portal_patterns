805e76ec6a8cd4dbb3f150b2bc8b37eae8dda622
Piotr Wieczorek <p@wie.cz>
2019-11-04 22:37:26 +0100

    Factor out some geo code from common.go to geo.go

--- lib/common.go
+++ lib/common.go
@@ -4,8 +4,6 @@ import "fmt"
 import "math"
 import "strings"
 
-import "github.com/golang/geo/r3"
-import "github.com/golang/geo/s1"
 import "github.com/golang/geo/s2"
 
 type portalIndex uint16
@@ -30,92 +28,6 @@ const invalidLength uint16 = math.MaxUint16
 type bestSolution struct {
 	Index  portalIndex
 	Length uint16
-}
-
-type triangleQuery struct {
-	aCrossB, cCrossA, bCrossC r3.Vector
-}
-
-func newTriangleQuery(a, b, c s2.Point) triangleQuery {
-	if !s2.Sign(a, b, c) {
-		a, c = c, a
-	}
-	aCrossB := a.Cross(b.Vector)
-	cCrossA := c.Cross(a.Vector)
-	bCrossC := b.Cross(c.Vector)
-	return triangleQuery{aCrossB, cCrossA, bCrossC}
-}
-
-func sign(aCrossB r3.Vector, c s2.Point) bool {
-	return aCrossB.Dot(c.Vector) > 0
-}
-
-func (t *triangleQuery) ContainsPoint(o s2.Point) bool {
-	if sign(t.aCrossB, o) && sign(t.cCrossA, o) && sign(t.bCrossC, o) {
-		return true
-	}
-	return false
-}
-
-type orderedCCWQuery struct {
-	a, o    s2.Point
-	aocSign bool
-	cCrossO r3.Vector
-}
-
-func newOrderedCCWQuery(a, c, o s2.Point) orderedCCWQuery {
-	aCrossO := a.Cross(o.Vector)
-	aocSign := sign(aCrossO, c)
-	cCrossO := c.Cross(o.Vector)
-	return orderedCCWQuery{a, o, aocSign, cCrossO}
-}
-
-func (t *orderedCCWQuery) Ordered(b s2.Point) bool {
-	if t.aocSign {
-		return sign(b.Cross(t.o.Vector), t.a) ||
-			sign(t.cCrossO, b)
-	}
-	return sign(b.Cross(t.o.Vector), t.a) &&
-		sign(t.cCrossO, b)
-}
-
-// triangleWedgeQuery helps to answer question wether a point is contained
-// inside a wedge which is a contained between semilines ab and ac, where angle
-// between ab and ac is < pi.
-type triangleWedgeQuery struct {
-	ccwQuery orderedCCWQuery
-}
-
-func newTriangleWedgeQuery(a, b, c s2.Point) triangleWedgeQuery {
-	if sign(a.Cross(b.Vector), c) {
-		return triangleWedgeQuery{newOrderedCCWQuery(b, c, a)}
-	}
-	return triangleWedgeQuery{newOrderedCCWQuery(c, b, a)}
-}
-
-func (t *triangleWedgeQuery) ContainsPoint(o s2.Point) bool {
-	return t.ccwQuery.Ordered(o)
-}
-
-type distanceQuery struct {
-	aCrossB s2.Point
-	invC2   float64
-}
-
-func newDistanceQuery(a, b s2.Point) distanceQuery {
-	aCrossB := a.PointCross(b)
-	return distanceQuery{aCrossB, 1.0 / aCrossB.Norm2()}
-}
-func (d *distanceQuery) ChordAngle(p s2.Point) s1.ChordAngle {
-	pDotC := p.Dot(d.aCrossB.Vector)
-	pDotC2 := pDotC * pDotC
-	cx := d.aCrossB.Cross(p.Vector)
-	qr := 1 - math.Sqrt(cx.Norm2()*d.invC2)
-	return s1.ChordAngle((pDotC2 * d.invC2) + (qr * qr))
-}
-func (d *distanceQuery) Distance(p s2.Point) s1.Angle {
-	return d.ChordAngle(p).Angle()
-
 }
 
 func portalsInsideWedge(portals []portalData, a, b, c portalData, result []portalData) []portalData {
@@ -157,14 +69,6 @@ func portalsInsideTriangle(portals []portalData, a, b, c portalData, result []po
 	return result
 }
 
-func triangleArea(p0, p1, p2 portalData) float64 {
-	return s2.GirardArea(p0.LatLng, p1.LatLng, p2.LatLng)
-}
-
-func distance(p0, p1 portalData) float64 {
-	return p0.LatLng.Sub(p1.LatLng.Vector).Norm()
-}
-
 func float64Min(v0, v1 float64) float64 {
 	if v0 < v1 {
 		return v0
@@ -183,9 +87,6 @@ func float32Min(v0, v1 float32) float32 {
 	}
 	return v1
 }
-
-const radiansToMeters = 2e+7 / math.Pi
-const unitAreaToSquareMeters = 5.1e+14
 
 func hasAllIndicesInTheTriple(indices []int, a, b, c int) bool {
 	for _, index := range indices {
