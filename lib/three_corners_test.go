cbf2b2f2a470aec48046d690efb6554df2b315a7
Piotr Wieczorek <p@wie.cz>
2019-12-10 22:44:12 +0100

    Check that number of corner changes is equal expected one in three corner test.

--- lib/three_corners_test.go
+++ lib/three_corners_test.go
@@ -4,42 +4,46 @@ import "testing"
 
 import "github.com/golang/geo/s2"
 
-type indexedPortalData struct {
-	Index int
-	Portal portalData
+type indexedPoint struct {
+	Index  int
+	LatLng s2.Point
 }
 
-func isCorrectThreeCorner(p [3]indexedPortalData, portals []indexedPortalData) bool {
-	if len(portals) == 0 {
+func isCorrectThreeCorner(p [3]indexedPoint, points []indexedPoint) bool {
+	if len(points) == 0 {
 		return true
 	}
-	triangle := newTriangleQuery(p[0].Portal.LatLng, p[1].Portal.LatLng, p[2].Portal.LatLng)
-	if !triangle.ContainsPoint(portals[0].Portal.LatLng) {
+	triangle := newTriangleQuery(p[0].LatLng, p[1].LatLng, p[2].LatLng)
+	if !triangle.ContainsPoint(points[0].LatLng) {
 		return false
 	}
-	p[portals[0].Index] = portals[0]
-	return isCorrectThreeCorner(p, portals[1:])
+	p[points[0].Index] = points[0]
+	return isCorrectThreeCorner(p, points[1:])
 }
 
-func checkValidThreeCornerResult(expectedLength int, portals []IndexedPortal, t *testing.T) {
+func checkValidThreeCornerResult(expectedLength int, expectedCornerChanges int, portals []IndexedPortal, t *testing.T) {
 	if len(portals) != expectedLength {
 		t.Errorf("Expected length %d, actual length %d", expectedLength, len(portals))
 	}
 	if portals[0].Index != 0 || portals[1].Index != 1 || portals[2].Index != 2 {
 		t.Errorf("Result is not correct three corner fielding")
 	}
-	indexedPortalsData := make([]indexedPortalData, 0, len(portals))
+	indexedPoints := make([]indexedPoint, 0, len(portals))
+	numIndexChanges := 0
 	for i, portal := range portals {
-		indexedPortalsData = append(indexedPortalsData, indexedPortalData{
-			Index: portal.Index,
-			Portal: portalData{
-				Index: portalIndex(i),
-				LatLng: s2.PointFromLatLng(portal.Portal.LatLng),
-			},
+		indexedPoints = append(indexedPoints, indexedPoint{
+			Index:  portal.Index,
+			LatLng: s2.PointFromLatLng(portal.Portal.LatLng),
 		})
+		if i > 3 && portals[i].Index != portals[i-1].Index {
+			numIndexChanges++
+		}
 	}
-	if !isCorrectThreeCorner([3]indexedPortalData{indexedPortalsData[0], indexedPortalsData[1], indexedPortalsData[2]}, indexedPortalsData[3:]) {
+	if !isCorrectThreeCorner([3]indexedPoint{indexedPoints[0], indexedPoints[1], indexedPoints[2]}, indexedPoints[3:]) {
 		t.Errorf("Result is not correct three corner fielding")
+	}
+	if numIndexChanges != expectedCornerChanges {
+		t.Errorf("Expected corner changes %d, actual %d", expectedCornerChanges, numIndexChanges)
 	}
 }
 
@@ -63,5 +67,5 @@ func TestThreeCorner(t *testing.T) {
 		t.FailNow()
 	}
 	threeCorner := LargestThreeCorner(portals0, portals1, portals2, func(int, int) {})
-	checkValidThreeCornerResult(16, threeCorner, t)
+	checkValidThreeCornerResult(16, 3, threeCorner, t)
 }
