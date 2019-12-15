793812ecd6269f4d859f6f4845c2b140f622df64
Piotr Wieczorek <p@wie.cz>
2019-12-15 22:24:26 +0100

    add commented out experimental version of the algorithm using sort and search instead of linear scans.

--- lib/flip_field.go
+++ lib/flip_field.go
@@ -1,5 +1,8 @@
 package lib
 
+//import "sort"
+//import "github.com/golang/geo/r2"
+
 // LargestFlipField -
 func LargestFlipField(portals []Portal, maxBackbonePortals int, numPortalLimit PortalLimit, numWorkers int, progressFunc func(int, int)) ([]Portal, []Portal) {
 	if numWorkers == 1 {
@@ -34,8 +37,14 @@ func newBestFlipFieldQuery(portals []portalData, maxBackbonePortals int, numPort
 	}
 }
 
-func (f *bestFlipFieldQuery) solve(bestSolution int) {
-}
+// Commented out code is for some experiments with using sort+search instead of linear scans
+// it's way slower now, but let's keep the code in this experimental branch if we ever want
+// to take a look at it
+/*type pointByAngle []r2.Point
+
+func (a pointByAngle) Len() int           { return len(a) }
+func (a pointByAngle) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a pointByAngle) Less(i, j int) bool { return angleLess(a[i], a[j]) }*/
 
 func (f *bestFlipFieldQuery) findBestFlipField(p0, p1 portalData) ([]portalData, []portalData) {
 	f.candidates = f.candidates[:0]
@@ -49,6 +58,7 @@ func (f *bestFlipFieldQuery) findBestFlipField(p0, p1 portalData) ([]portalData,
 		f.candidates = append(f.candidates, portal)
 	}
 	flipPortals := f.candidates
+//	flipPortalAngles := make([]r2.Point, 0, len(flipPortals))
 	f.backbone = append(f.backbone[:0], p0, p1)
 	for {
 		if len(f.backbone) >= f.maxBackbonePortals {
@@ -64,8 +74,52 @@ func (f *bestFlipFieldQuery) findBestFlipField(p0, p1 portalData) ([]portalData,
 		bestCandidate := -1
 		bestInsertPosition := -1
 		for i, candidate := range f.candidates {
+			/*flipPortalAngles = flipPortalAngles[:0]
+			for _, portal := range flipPortals {
+				if portal.Index != candidate.Index {
+					flipPortalAngles = append(flipPortalAngles, portal.LatLng.Sub(candidate.LatLng))
+				}
+			}
+			if len(flipPortalAngles)*(2*len(f.backbone)+1) <= bestNumFields {
+				continue
+			}
+			sort.Sort(pointByAngle(flipPortalAngles))*/
 			for pos := 1; pos < len(f.backbone); pos++ {
 				numFlipPortals := numPortalsLeftOfTwoLines(flipPortals, f.backbone[pos-1], candidate, f.backbone[pos])
+/*				b0, b1 := f.backbone[pos-1].LatLng, f.backbone[pos].LatLng
+				var num0, num1, numFlipPortals int
+				var ac bool
+				if Sign(b0, b1, candidate.LatLng) > 0 {
+					a0, a1 := candidate.LatLng.Sub(b0), candidate.LatLng.Sub(b1)
+					num0 = sort.Search(len(flipPortalAngles), func(i int) bool { return angleLess(a0, flipPortalAngles[i]) })
+					num1 = sort.Search(len(flipPortalAngles), func(i int) bool { return angleLess(a1, flipPortalAngles[i]) })
+					ac = angleLess(a0, a1)
+					if !ac && num1 == num0 {
+						numFlipPortals = len(flipPortalAngles)
+					} else {
+						numFlipPortals = num1 - num0
+						if numFlipPortals < 0 {
+							numFlipPortals += len(flipPortalAngles)
+						}
+					}
+				} else {
+					a0, a1 := b0.Sub(candidate.LatLng), b1.Sub(candidate.LatLng)
+					num0 = sort.Search(len(flipPortalAngles), func(i int) bool { return angleLess(a0, flipPortalAngles[i]) })
+					num1 = sort.Search(len(flipPortalAngles), func(i int) bool { return angleLess(a1, flipPortalAngles[i]) })
+					ac = angleLess(a0, a1)
+					numFlipPortals = num1 - num0
+					if !ac && numFlipPortals == len(flipPortalAngles) {
+						numFlipPortals = 0
+					}
+					if !ac && numFlipPortals < 0 {
+						numFlipPortals = -numFlipPortals
+					} else if ac {
+						numFlipPortals = len(flipPortals) - numFlipPortals
+					}
+					if numFlipPortals < 0 {
+						numFlipPortals += len(flipPortalAngles)
+					}
+				}*/
 				numFields := numFlipPortals * (2*len(f.backbone) + 1)
 				if numFields > bestNumFields {
 					bestNumFields = numFields
@@ -83,6 +137,12 @@ func (f *bestFlipFieldQuery) findBestFlipField(p0, p1 portalData) ([]portalData,
 		f.candidates[bestCandidate], f.candidates[len(f.candidates)-1] =
 			f.candidates[len(f.candidates)-1], f.candidates[bestCandidate]
 		f.candidates = f.candidates[:len(f.candidates)-1]
+		// If candidates and flipPortals were the same slice, we must shrink it before partitioning
+		// otherwise reordering may bring the removed object back among candidates and confuse the algorithm
+		// (and also possibly remove a proper candidate).
+		if len(flipPortals) > len(f.candidates) {
+			flipPortals = flipPortals[:len(flipPortals)-1]
+		}
 		flipPortals = partitionPortalsLeftOfLine(flipPortals, f.backbone[bestInsertPosition-1], f.backbone[bestInsertPosition])
 		flipPortals = partitionPortalsLeftOfLine(flipPortals, f.backbone[bestInsertPosition], f.backbone[bestInsertPosition+1])
 	}
