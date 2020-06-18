b1f7e065a419fc64918774722588f96a2a295389
Piotr Wieczorek <p@wie.cz>
2020-06-18 10:54:54 +0200

    Faster drone flights when only end portal is specified.

---
+++ lib/fifo.go
@@ -0,0 +1,38 @@
+package lib
+
+func reverse(s []portalIndex) {
+	for i := len(s) / 2 - 1; i >= 0; i-- {
+		opp := len(s)-1-i
+		s[i], s[opp] = s[opp], s[i]
+	}
+}
+
+type fifo struct {
+	front, back []portalIndex
+}
+
+func (f *fifo) Reset() {
+	f.front = f.back[:0]
+	f.back = f.back[:0]
+}
+func (f *fifo) Empty() bool {
+	return len(f.front)+len(f.back) == 0
+}
+func (f *fifo) Enqueue(p portalIndex) {
+	f.back = append(f.back, p)
+}
+func (f *fifo) Dequeue() portalIndex {
+	lenFirst := len(f.front)
+	if lenFirst == 0 {
+		lenFirst = len(f.back)
+		if lenFirst == 0 {
+			panic("Empty queue")
+		}
+		f.front, f.back = f.back, f.front
+		reverse(f.front)
+	}
+	r := f.front[lenFirst-1]
+	f.front = f.front[:lenFirst-1]
+	return r
+}
+
