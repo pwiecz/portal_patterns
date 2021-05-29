1148c097acf2eb31a4a5bc6b99112d1e0cb910fd
Piotr Wieczorek <p@wie.cz>
2021-05-29 18:58:34 +0200

    Add a missing file

---
+++ gui/pattern.go
@@ -0,0 +1,22 @@
+package main
+
+import "image/color"
+
+
+type menuItem struct {
+	label string
+	callback func()
+}
+type menu struct {
+	header string
+	items []menuItem
+}
+
+type pattern interface {
+	onSearch()
+	portalColor(string) color.Color
+	portalLabel(string) string
+	solutionString() string
+	onReset()
+	contextMenu() *menu//x, y int)
+}
