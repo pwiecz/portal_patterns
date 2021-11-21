76316df45e68b5c32f0a942f76059b1c7dbadf9e
Piotr Wieczorek <p@wie.cz>
2021-11-21 21:40:22 +0100

    Add a simple cobweb benchmark

--- lib/cobweb_test.go
+++ lib/cobweb_test.go
@@ -1,6 +1,11 @@
 package lib
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/golang/geo/s2"
+)
 
 func isCorrectCobweb(p0, p1, p2 portalData, portals []portalData) bool {
 	if len(portals) == 0 {
@@ -37,3 +42,44 @@ func TestCobweb(t *testing.T) {
 	cobweb := LargestCobweb(portals, []int{}, func(int, int) {})
 	checkValidCobwebResult(22, cobweb, t)
 }
+
+func appendPortalsBetween(depth int, p Portal, ll s2.LatLng, portals []Portal) []Portal {
+	diff := s2.PointFromLatLng(p.LatLng).Sub(s2.PointFromLatLng(ll).Vector)
+	for i := 1; i < depth; i++ {
+		midPoint := s2.PointFromLatLng(p.LatLng).Add(
+			diff.Mul(float64(i) / float64(depth)))
+		midLL := s2.LatLngFromPoint(s2.Point{midPoint})
+		midPortal := Portal{Guid: p.Guid + fmt.Sprintf("%d", i), LatLng: midLL}
+		portals = append(portals, midPortal)
+	}
+	return portals
+}
+
+func generateCobwebPortals(depth int) []Portal {
+	ll0 := s2.LatLngFromDegrees(20, 20)
+	ll1 := s2.LatLngFromDegrees(20, 22)
+	ll2 := s2.LatLngFromDegrees(21, 21)
+	portals := []Portal{
+		Portal{Guid: "a", LatLng: ll0},
+		Portal{Guid: "b", LatLng: ll1},
+		Portal{Guid: "c", LatLng: ll2}}
+	midLL := s2.LatLngFromDegrees((20+20+21)/3., (20+22+21)/3.)
+	portals = appendPortalsBetween(depth, portals[0], midLL, portals)
+	portals = appendPortalsBetween(depth, portals[1], midLL, portals)
+	portals = appendPortalsBetween(depth, portals[2], midLL, portals)
+	return portals
+}
+
+func benchmarkCobweb(depth int, b *testing.B) {
+	portals := generateCobwebPortals(depth)
+	for n := 0; n < b.N; n++ {
+		res := LargestCobweb(portals, []int{}, func(int, int) {})
+		if len(res) != len(portals) {
+			panic(fmt.Sprintf("%d != %d: %v", len(res), len(portals), res))
+		}
+	}
+}
+
+func BenchmarkCobweb20(b *testing.B) { benchmarkCobweb(20, b) }
+func BenchmarkCobweb30(b *testing.B) { benchmarkCobweb(30, b) }
+func BenchmarkCobweb40(b *testing.B) { benchmarkCobweb(40, b) }
