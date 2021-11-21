e648eda4985c840fcae6c08b6261ef74bc2f96d6
Piotr Wieczorek <p@wie.cz>
2021-11-21 21:05:59 +0100

    Add a simple homogeneous benchmark

--- lib/homogeneous_test.go
+++ lib/homogeneous_test.go
@@ -110,3 +110,68 @@ func TestHomogeneousPretty(t *testing.T) {
 	result, depth := DeepestHomogeneous(portals, HomogeneousSpreadAround{}, HomogeneousMaxDepth(6), HomogeneousLargestArea{}, HomogeneousNumWorkers(6))
 	checkValidHomogeneousResult(5, result, depth, t)
 }
+
+func appendMidPortals(depth int, p0, p1, p2 Portal, portals []Portal) []Portal {
+	if depth <= 0 {
+		return portals
+	}
+	midPoint := s2.PointFromLatLng(p0.LatLng).Add(
+		s2.PointFromLatLng(p1.LatLng).Vector).Add(
+		s2.PointFromLatLng(p2.LatLng).Vector).Mul(1. / 3.)
+	midLL := s2.LatLngFromPoint(s2.Point{midPoint})
+	midPortal := Portal{Guid: p0.Guid + string(rune('0'+depth)), LatLng: midLL}
+	portals = append(portals, midPortal)
+	portals = appendMidPortals(depth-1, p0, p1, midPortal, portals)
+	portals = appendMidPortals(depth-1, p1, p2, midPortal, portals)
+	portals = appendMidPortals(depth-1, p2, p0, midPortal, portals)
+	return portals
+}
+
+func createHomogeneousPortals(depth int) []Portal {
+	ll0 := s2.LatLngFromDegrees(20, 20)
+	ll1 := s2.LatLngFromDegrees(20, 22)
+	ll2 := s2.LatLngFromDegrees(21, 21)
+	portals := []Portal{
+		Portal{Guid: "a", LatLng: ll0},
+		Portal{Guid: "b", LatLng: ll1},
+		Portal{Guid: "c", LatLng: ll2}}
+	return appendMidPortals(depth-1, portals[0], portals[1], portals[2], portals)
+}
+
+func benchmarkHomogeneous(depth int, b *testing.B) {
+	portals := createHomogeneousPortals(depth)
+	for n := 0; n < b.N; n++ {
+		_, resDepth := DeepestHomogeneous(portals, HomogeneousMaxDepth(6), HomogeneousLargestArea{}, HomogeneousNumWorkers(7))
+		if depth != int(resDepth) {
+			panic(resDepth)
+		}
+	}
+}
+func BenchmarkHomogeneous4(b *testing.B) { benchmarkHomogeneous(4, b) }
+func BenchmarkHomogeneous5(b *testing.B) { benchmarkHomogeneous(5, b) }
+func BenchmarkHomogeneous6(b *testing.B) { benchmarkHomogeneous(6, b) }
+
+func benchmarkHomogeneousPretty(depth int, b *testing.B) {
+	portals := createHomogeneousPortals(depth)
+	for n := 0; n < b.N; n++ {
+		_, resDepth := DeepestHomogeneous(portals, HomogeneousSpreadAround{}, HomogeneousMaxDepth(6), HomogeneousLargestArea{}, HomogeneousNumWorkers(6))
+		if depth != int(resDepth) {
+			panic(resDepth)
+		}
+	}
+}
+func BenchmarkHomogeneousPretty4(b *testing.B) { benchmarkHomogeneousPretty(4, b) }
+func BenchmarkHomogeneousPretty5(b *testing.B) { benchmarkHomogeneousPretty(5, b) }
+func BenchmarkHomogeneousPretty6(b *testing.B) { benchmarkHomogeneousPretty(6, b) }
+
+func benchmarkHomogeneousPure(depth int, b *testing.B) {
+	portals := createHomogeneousPortals(depth)
+	for n := 0; n < b.N; n++ {
+		_, resDepth := DeepestHomogeneous(portals, HomogeneousPure(true), HomogeneousMaxDepth(6), HomogeneousLargestArea{}, HomogeneousNumWorkers(6))
+		if depth != int(resDepth) {
+			panic(resDepth)
+		}
+	}
+}
+func BenchmarkHomogeneousPure4(b *testing.B) { benchmarkHomogeneousPure(4, b) }
+func BenchmarkHomogeneousPure5(b *testing.B) { benchmarkHomogeneousPure(5, b) }
